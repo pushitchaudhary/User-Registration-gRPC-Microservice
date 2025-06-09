8c81a4ade154ce3ffbbb7c3f3f005fddeac1fcbf
pushit chaudhary <pushitcoc@gmail.com>
2025-06-09 18:53:39 +0545

    proto path and server file setup

---
+++ Email_Service/src/server.ts
@@ -0,0 +1,6 @@
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
+import * as path from 'path'
+
+const PORTO_PATH = path.resolve(__dirname, '../src/proto/email.proto')
+
